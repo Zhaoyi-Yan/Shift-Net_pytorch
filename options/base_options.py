0e4b054a34735a73adede029cef8eda6d0d73ea8
Zhaoyi-Yan <yanzhaoyi@outlook.com>
2018-07-15 17:24:13 +0800

    fix #2

--- options/base_options.py
+++ options/base_options.py
@@ -33,7 +33,8 @@ class BaseOptions():
         parser.add_argument('--display_winsize', type=int, default=256,  help='display window size')
         parser.add_argument('--display_id', type=int, default=1, help='window id of the web display')
         parser.add_argument('--display_port', type=int, default=8097, help='visdom port of the web display')
-
+        parser.add_argument('--suffix', default='', type=str, help='customized suffix: opt.name = opt.name + suffix: e.g., {model}_{which_model_netG}_size{loadSize}')
+        
         parser.add_argument('--use_dropout', action='store_true', help='use dropout for the generator') #it means if using '--use_dropout then this option is true.
         parser.add_argument('--max_dataset_size', type=int, default=float("inf"), help='Maximum number of samples allowed per dataset. If the dataset directory contains more than max_dataset_size, only a subset is loaded.')
         parser.add_argument('--resize_or_crop', type=str, default='resize_and_crop', help='scaling and cropping of images at load time [resize_and_crop|crop|scale_width]')
