4a2b0aaac69719d0ed3563dc0d7b7a6756241e1b
Hungryof <ziquanlanyue@163.com>
2018-08-14 17:17:51 +0800

    minor fix

--- models/networks.py
+++ models/networks.py
@@ -20,7 +20,7 @@ def get_norm_layer(norm_type='instance'):
     if norm_type == 'batch':
         norm_layer = functools.partial(nn.BatchNorm2d, affine=True)
     elif norm_type == 'instance':
-        norm_layer = functools.partial(nn.InstanceNorm2d, affine=True)
+        norm_layer = functools.partial(nn.InstanceNorm2d, affine=False)
     elif norm_type == 'none':
         norm_layer = None
     else:
@@ -48,20 +48,20 @@ def init_weights(net, init_type='normal', gain=0.02):
         classname = m.__class__.__name__
         if hasattr(m, 'weight') and (classname.find('Conv') != -1 or classname.find('Linear') != -1):
             if init_type == 'normal':
-                init.normal_(m.weight.data, 0.0, gain)
+                init.normal(m.weight.data, 0.0, gain)
             elif init_type == 'xavier':
-                init.xavier_normal_(m.weight.data, gain=gain)
+                init.xavier_normal(m.weight.data, gain=gain)
             elif init_type == 'kaiming':
-                init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
+                init.kaiming_normal(m.weight.data, a=0, mode='fan_in')
             elif init_type == 'orthogonal':
-                init.orthogonal_(m.weight.data, gain=gain)
+                init.orthogonal(m.weight.data, gain=gain)
             else:
                 raise NotImplementedError('initialization method [%s] is not implemented' % init_type)
             if hasattr(m, 'bias') and m.bias is not None:
-                init.constant_(m.bias.data, 0.0)
+                init.constant(m.bias.data, 0.0)
         elif classname.find('BatchNorm2d') != -1:
-            init.normal_(m.weight.data, 1.0, gain)
-            init.constant_(m.bias.data, 0.0)
+            init.normal(m.weight.data, 1.0, gain)
+            init.constant(m.bias.data, 0.0)
 
     print('initialize network with %s' % init_type)
     net.apply(init_func)
@@ -70,7 +70,7 @@ def init_weights(net, init_type='normal', gain=0.02):
 def init_net(net, init_type='normal', init_gain=0.02, gpu_ids=[]):
     if len(gpu_ids) > 0:
         assert(torch.cuda.is_available())
-        net.to(gpu_ids[0])
+        net.cuda(gpu_ids[0])
         net = torch.nn.DataParallel(net, gpu_ids)
     init_weights(net, init_type, gain=init_gain)
     return net
