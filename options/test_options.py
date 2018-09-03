17e7b653a34ae8f20a70dc596ea3be8642127fde
Zhaoyi-Yan <yanzhaoyi@outlook.com>
2018-09-03 09:57:52 +0800

    Fix initialize() takes exactly 1 argument
    initialize() takes exactly 1 argument (2 given)

--- options/test_options.py
+++ options/test_options.py
@@ -2,7 +2,7 @@ from .base_options import BaseOptions
 
 
 class TestOptions(BaseOptions):
-    def initialize(self):
+    def initialize(self, parser):
         parser = BaseOptions.initialize(self, parser)
         parser.add_argument('--ntest', type=int, default=float("inf"), help='# of test examples.')
         parser.add_argument('--results_dir', type=str, default='./results/', help='saves results here.')
