d108a24ddc9aae197af59f584a46b74a5c9217dd
Zhaoyi-Yan <yanzhaoyi@outlook.com>
2018-12-24 18:48:15 +0800

    compatible for winodws

--- test.py
+++ test.py
@@ -6,34 +6,35 @@ from models import create_model
 from util.visualizer import Visualizer
 from util import html
 
-opt = TestOptions().parse()
-opt.nThreads = 1   # test code only supports nThreads = 1
-opt.batchSize = 1  # test code only supports batchSize = 1
-opt.serial_batches = True  # no shuffle
-opt.no_flip = True  # no flip
-opt.display_id = -1 # no visdom display
-opt.loadSize = opt.fineSize  # Do not scale!
+if __name__ == "__main__":
+    opt = TestOptions().parse()
+    opt.nThreads = 1   # test code only supports nThreads = 1
+    opt.batchSize = 1  # test code only supports batchSize = 1
+    opt.serial_batches = True  # no shuffle
+    opt.no_flip = True  # no flip
+    opt.display_id = -1 # no visdom display
+    opt.loadSize = opt.fineSize  # Do not scale!
 
-data_loader = CreateDataLoader(opt)
-dataset = data_loader.load_data()
-model = create_model(opt)
+    data_loader = CreateDataLoader(opt)
+    dataset = data_loader.load_data()
+    model = create_model(opt)
 
-visualizer = Visualizer(opt)
-# create website
-web_dir = os.path.join(opt.results_dir, opt.name, '%s_%s' % (opt.phase, opt.which_epoch))
-webpage = html.HTML(web_dir, 'Experiment = %s, Phase = %s, Epoch = %s' % (opt.name, opt.phase, opt.which_epoch))
-# test
-for i, data in enumerate(dataset):
-    if i >= opt.how_many:
-        break
-    t1 = time.time()
-    model.set_input(data)
-    model.test()
-    t2 = time.time()
-    print(t2-t1)
-    visuals = model.get_current_visuals()
-    img_path = model.get_image_paths()
-    print('process image... %s' % img_path)
-    visualizer.save_images(webpage, visuals, img_path)
+    visualizer = Visualizer(opt)
+    # create website
+    web_dir = os.path.join(opt.results_dir, opt.name, '%s_%s' % (opt.phase, opt.which_epoch))
+    webpage = html.HTML(web_dir, 'Experiment = %s, Phase = %s, Epoch = %s' % (opt.name, opt.phase, opt.which_epoch))
+    # test
+    for i, data in enumerate(dataset):
+        if i >= opt.how_many:
+            break
+        t1 = time.time()
+        model.set_input(data)
+        model.test()
+        t2 = time.time()
+        print(t2-t1)
+        visuals = model.get_current_visuals()
+        img_path = model.get_image_paths()
+        print('process image... %s' % img_path)
+        visualizer.save_images(webpage, visuals, img_path)
 
-webpage.save()
+    webpage.save()
--- train.py
+++ train.py
@@ -4,68 +4,69 @@ from data.data_loader import CreateDataLoader
 from models import create_model
 from util.visualizer import Visualizer
 
-opt = TrainOptions().parse()
-data_loader = CreateDataLoader(opt)
-dataset = data_loader.load_data()
-dataset_size = len(data_loader)
-print('#training images = %d' % dataset_size)
+if __name__ == "__main__":
+    opt = TrainOptions().parse()
+    data_loader = CreateDataLoader(opt)
+    dataset = data_loader.load_data()
+    dataset_size = len(data_loader)
+    print('#training images = %d' % dataset_size)
 
-model = create_model(opt)
-visualizer = Visualizer(opt)
+    model = create_model(opt)
+    visualizer = Visualizer(opt)
 
-total_steps = 0
+    total_steps = 0
 
-for epoch in range(opt.epoch_count, opt.niter + opt.niter_decay + 1):
-    epoch_start_time = time.time()
-    iter_data_time = time.time()
-    epoch_iter = 0
+    for epoch in range(opt.epoch_count, opt.niter + opt.niter_decay + 1):
+        epoch_start_time = time.time()
+        iter_data_time = time.time()
+        epoch_iter = 0
 
-    for i, data in enumerate(dataset):
-        iter_start_time = time.time()
-        if total_steps % opt.print_freq == 0:
-            t_data = iter_start_time - iter_data_time
-        visualizer.reset()
-        total_steps += opt.batchSize
-        epoch_iter += opt.batchSize
+        for i, data in enumerate(dataset):
+            iter_start_time = time.time()
+            if total_steps % opt.print_freq == 0:
+                t_data = iter_start_time - iter_data_time
+            visualizer.reset()
+            total_steps += opt.batchSize
+            epoch_iter += opt.batchSize
 
-        model.set_input(data) # it not only sets the input data with mask, but also sets the latent mask.
-        model.set_gt_latent()
+            model.set_input(data) # it not only sets the input data with mask, but also sets the latent mask.
+            model.set_gt_latent()
 
-        # Additonal, should set it before 'optimize_parameters()'.
-        if total_steps % opt.display_freq == 0:
-            if opt.show_flow:
-                model.set_show_map_true()
+            # Additonal, should set it before 'optimize_parameters()'.
+            if total_steps % opt.display_freq == 0:
+                if opt.show_flow:
+                    model.set_show_map_true()
 
-        model.optimize_parameters()
+            model.optimize_parameters()
 
-        if total_steps % opt.display_freq == 0:
-            save_result = total_steps % opt.update_html_freq == 0
-            if opt.show_flow:
-                model.set_flow_src()
-                model.set_show_map_false()
-            visuals = model.get_current_visuals()
-            visualizer.display_current_results(model.get_current_visuals(), epoch, save_result)
+            if total_steps % opt.display_freq == 0:
+                save_result = total_steps % opt.update_html_freq == 0
+                if opt.show_flow:
+                    model.set_flow_src()
+                    model.set_show_map_false()
+                visuals = model.get_current_visuals()
+                visualizer.display_current_results(model.get_current_visuals(), epoch, save_result)
 
-        if total_steps % opt.print_freq == 0:
-            losses = model.get_current_losses()
-            t = (time.time() - iter_start_time) / opt.batchSize
-            visualizer.print_current_losses(epoch, epoch_iter, losses, t, t_data)
-            if opt.display_id > 0:
-                visualizer.plot_current_losses(epoch, float(epoch_iter) / dataset_size, opt, losses)
+            if total_steps % opt.print_freq == 0:
+                losses = model.get_current_losses()
+                t = (time.time() - iter_start_time) / opt.batchSize
+                visualizer.print_current_losses(epoch, epoch_iter, losses, t, t_data)
+                if opt.display_id > 0:
+                    visualizer.plot_current_losses(epoch, float(epoch_iter) / dataset_size, opt, losses)
 
-        if total_steps % opt.save_latest_freq == 0:
-            print('saving the latest model (epoch %d, total_steps %d)' %
+            if total_steps % opt.save_latest_freq == 0:
+                print('saving the latest model (epoch %d, total_steps %d)' %
+                        (epoch, total_steps))
+                model.save_networks('latest')
+
+            iter_data_time = time.time()
+        if epoch % opt.save_epoch_freq == 0:
+            print('saving the model at the end of epoch %d, iters %d' %
                     (epoch, total_steps))
             model.save_networks('latest')
+            if not opt.only_lastest:
+                model.save_networks(epoch)
 
-        iter_data_time = time.time()
-    if epoch % opt.save_epoch_freq == 0:
-        print('saving the model at the end of epoch %d, iters %d' %
-                (epoch, total_steps))
-        model.save_networks('latest')
-        if not opt.only_lastest:
-            model.save_networks(epoch)
-
-    print('End of epoch %d / %d \t Time Taken: %d sec' %
-            (epoch, opt.niter + opt.niter_decay, time.time() - epoch_start_time))
-    model.update_learning_rate()
+        print('End of epoch %d / %d \t Time Taken: %d sec' %
+                (epoch, opt.niter + opt.niter_decay, time.time() - epoch_start_time))
+        model.update_learning_rate()
