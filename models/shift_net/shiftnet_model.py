9ce1dbccbd650b86aa5e40ed7648d962b8d1e9cc
Zhaoyi-Yan <yanzhaoyi@outlook.com>
2018-12-24 19:22:50 +0800

    fix for batch bug

--- models/shift_net/shiftnet_model.py
+++ models/shift_net/shiftnet_model.py
@@ -149,8 +149,7 @@ class ShiftNetModel(BaseModel):
         if self.opt.add_mask2input:
             # make it 4 dimensions.
             # Mention: the extra dim, the masked part is filled with 0, non-mask part is filled with 1.
-            real_A = torch.cat((real_A, (1 - self.mask_global).expand(self.opt.batchSize, 1, \
-                                     self.opt.fineSize, self.opt.fineSize).type_as(real_A)), dim=1)
+            real_A = torch.cat((real_A, (1 - self.mask_global).expand(real_A.size(0), 1, real_A.size(2), real_A.size(3)).type_as(real_A)), dim=1)
 
         self.real_A = real_A
         self.real_B = real_B
@@ -182,8 +181,7 @@ class ShiftNetModel(BaseModel):
             if self.opt.add_mask2input:
                 # make it 4 dimensions.
                 # Mention: the extra dim, the masked part is filled with 0, non-mask part is filled with 1.
-                real_B = torch.cat([self.real_B, (1 - self.mask_global).expand(self.opt.batchSize, 1, \
-                           self.opt.fineSize, self.opt.fineSize).type_as(self.real_B)], dim=1)
+                real_B = torch.cat([self.real_B, (1 - self.mask_global).expand(self.real_B.size(0), 1, self.real_B.size(2), self.real_B.size(3)).type_as(self.real_B)], dim=1)
             else:
                 real_B = self.real_B
             self.netG(real_B) # input ground truth
