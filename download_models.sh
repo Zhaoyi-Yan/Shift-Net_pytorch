2989bb724b3c70d32a6c1e882ea7de356c9b3c54
Zhaoyi-Yan <yanzhaoyi@outlook.com>
2019-07-20 08:35:47 +0800

    add pretrained models url

--- download_models.sh
+++ download_models.sh
@@ -1,2 +1,6 @@
 # face model (Trained on CelebaHQ-256, the first 2k images are for testing, the rest are for training.)
 wget -c https://drive.google.com/open?id=1Y4T6dkff3cIz1QRQ9mGhCORskzwPA-Bl
+# face random mask model
+wget -c https://drive.google.com/open?id=1fpQrcHoW0wsb1CToMkXt0_WWhULR11U4
+# paris random mask model
+wget -c https://drive.google.com/open?id=14MzixaqYUdJNL5xGdVhSKI9jOfvGdr3M
