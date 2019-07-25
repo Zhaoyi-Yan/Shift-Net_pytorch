46befd839cd09a9fda48c116f31ed023629d2fbb
Zhaoyi-Yan <yanzhaoyi@outlook.com>
2019-07-25 11:01:32 +0800

    add paris center mask

--- download_models.sh
+++ download_models.sh
@@ -4,3 +4,5 @@ wget -c https://drive.google.com/open?id=1Y4T6dkff3cIz1QRQ9mGhCORskzwPA-Bl
 wget -c https://drive.google.com/open?id=1fpQrcHoW0wsb1CToMkXt0_WWhULR11U4
 # paris random mask model
 wget -c https://drive.google.com/open?id=14MzixaqYUdJNL5xGdVhSKI9jOfvGdr3M
+# paris center mask model
+wget -c https://drive.google.com/open?id=1nDkCdsqUdiEXfSjZ_P915gWeZELK0fo_
