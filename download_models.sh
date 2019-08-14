4287fcd071656545bdf8a65fcc473a5373dd23b8
Zhaoyi-Yan <yanzhaoyi@outlook.com>
2019-08-14 09:29:39 +0800

    Update download_models.sh

--- download_models.sh
+++ download_models.sh
@@ -1,5 +1,5 @@
 # face model (Trained on CelebaHQ-256, the first 2k images are for testing, the rest are for training.)
-wget -c https://drive.google.com/open?id=1Y4T6dkff3cIz1QRQ9mGhCORskzwPA-Bl
+# Have trained, uploading.
 # face random mask model
 wget -c https://drive.google.com/open?id=1fpQrcHoW0wsb1CToMkXt0_WWhULR11U4
 # paris random mask model
