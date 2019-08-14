76858f0ecd761fc882ba37c1e4efd09d829b95d5
Zhaoyi-Yan <yanzhaoyi@outlook.com>
2019-08-14 09:49:51 +0800

    Train the new model for face center

--- download_models.sh
+++ download_models.sh
@@ -1,5 +1,5 @@
 # face model (Trained on CelebaHQ-256, the first 2k images are for testing, the rest are for training.)
-# Have trained, uploading.
+wget -c https://drive.google.com/open?id=1qvsWHVO9iXpEAPtwyRB25mklTmD0jgPV
 # face random mask model
 wget -c https://drive.google.com/open?id=1fpQrcHoW0wsb1CToMkXt0_WWhULR11U4
 # paris random mask model
