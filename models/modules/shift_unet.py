6388a07c46148f09c4f6dead249d1e417b60677f
Zhaoyi-Yan <yanzhaoyi@outlook.com>
2019-11-27 15:43:18 +0800

    Update shift_unet.py

--- models/modules/shift_unet.py
+++ models/modules/shift_unet.py
@@ -9,9 +9,6 @@ from models.shift_net.InnerCos import InnerCos
 # For res shift
 from models.res_shift_net.innerResShiftTriple import InnerResShiftTriple
 
-# For pixel soft shift
-from models.soft_shift_net.innerSoftShiftTriple import InnerSoftShiftTriple
-
 # For patch patch shift
 from models.patch_soft_shift.innerPatchSoftShiftTriple import InnerPatchSoftShiftTriple 
 
@@ -245,115 +242,6 @@ class ResUnetSkipConnectionBlock(nn.Module):
             if h != x_latter.size(2) or w != x_latter.size(3):
                 x_latter = F.interpolate(x_latter, (h, w), mode='bilinear')
             return torch.cat([x_latter, x], 1)  # cat in the C channel
-
-################################### ***************************  #####################################
-###################################        Soft pixel shift      #####################################
-################################### ***************************  #####################################
-class SoftUnetGeneratorShiftTriple(nn.Module):
-    def __init__(self, input_nc, output_nc, num_downs, opt, innerCos_list, shift_list, mask_global, ngf=64,
-                 norm_layer=nn.BatchNorm2d, use_spectral_norm=False):
-        super(SoftUnetGeneratorShiftTriple, self).__init__()
-
-        # construct unet structure
-        unet_block = UnetSkipConnectionBlock(ngf * 8, ngf * 8, input_nc=None, submodule=None, norm_layer=norm_layer,
-                                             innermost=True, use_spectral_norm=use_spectral_norm)
-        print(unet_block)
-        for i in range(num_downs - 5):  # The innner layers number is 3 (sptial size:512*512), if unet_256.
-            unet_block = UnetSkipConnectionBlock(ngf * 8, ngf * 8, input_nc=None, submodule=unet_block,
-                                                 norm_layer=norm_layer, use_spectral_norm=use_spectral_norm)
-        unet_block = UnetSkipConnectionBlock(ngf * 4, ngf * 8, input_nc=None, submodule=unet_block,
-                                             norm_layer=norm_layer, use_spectral_norm=use_spectral_norm)
-
-        unet_shift_block = SoftUnetSkipConnectionBlock(ngf * 2, ngf * 4, opt, innerCos_list, shift_list,
-                                                                 mask_global, input_nc=None, \
-                                                                 submodule=unet_block,
-                                                                 norm_layer=norm_layer, use_spectral_norm=use_spectral_norm, layer_to_last=3)  # passing in unet_shift_block
-        unet_block = UnetSkipConnectionBlock(ngf, ngf * 2, input_nc=None, submodule=unet_shift_block,
-                                             norm_layer=norm_layer, use_spectral_norm=use_spectral_norm)
-        unet_block = UnetSkipConnectionBlock(output_nc, ngf, input_nc=input_nc, submodule=unet_block, outermost=True,
-                                             norm_layer=norm_layer, use_spectral_norm=use_spectral_norm)
-
-        self.model = unet_block
-
-    def forward(self, input):
-        return self.model(input)
-
-
-# construct network from the inside to the outside.
-# Defines the submodule with skip connection.
-# X -------------------identity---------------------- X
-#   |-- downsampling -- |submodule| -- upsampling --|
-class SoftUnetSkipConnectionBlock(nn.Module):
-    def __init__(self, outer_nc, inner_nc, opt, innerCos_list, shift_list, mask_global, input_nc, \
-                 submodule=None, outermost=False, innermost=False, norm_layer=nn.BatchNorm2d, use_spectral_norm=False, layer_to_last=3):
-        super(SoftUnetSkipConnectionBlock, self).__init__()
-        self.outermost = outermost
-        if input_nc is None:
-            input_nc = outer_nc
-
-        downconv = spectral_norm(nn.Conv2d(input_nc, inner_nc, kernel_size=4,
-                             stride=2, padding=1), use_spectral_norm)
-        downrelu = nn.LeakyReLU(0.2, True)
-        downnorm = norm_layer(inner_nc)
-        uprelu = nn.ReLU(True)
-        upnorm = norm_layer(outer_nc)
-
-        # As the downconv layer is outer_nc in and inner_nc out.
-        # So the shift define like this:
-        shift = InnerSoftShiftTriple(opt.shift_sz, opt.stride, opt.mask_thred, opt.triple_weight, layer_to_last=layer_to_last)
-
-        shift.set_mask(mask_global)
-        shift_list.append(shift)
-
-        # Add latent constraint
-        # Then add the constraint to the constrain layer list!
-        innerCos = InnerCos(strength=opt.strength, skip=opt.skip, layer_to_last=layer_to_last)
-        innerCos.set_mask(mask_global)  # Here we need to set mask for innerCos layer too.
-        innerCos_list.append(innerCos)
-
-        # Different position only has differences in `upconv`
-        # for the outermost, the special is `tanh`
-        if outermost:
-            upconv = spectral_norm(nn.ConvTranspose2d(inner_nc * 2, outer_nc,
-                                        kernel_size=4, stride=2,
-                                        padding=1), use_spectral_norm)
-            down = [downconv]
-            up = [uprelu, upconv, nn.Tanh()]
-            model = down + [submodule] + up
-            # for the innermost, the special is `inner_nc` instead of `inner_nc*2`
-        elif innermost:
-            upconv = spectral_norm(nn.ConvTranspose2d(inner_nc, outer_nc,
-                                        kernel_size=4, stride=2,
-                                        padding=1), use_spectral_norm)
-            down = [downrelu, downconv]  # for the innermost, no submodule, and delete the bn
-            up = [uprelu, upconv, upnorm]
-            model = down + up
-            # else, the normal
-        else:
-            # shift triple differs in here. It is `*3` not `*2`.
-            upconv = spectral_norm(nn.ConvTranspose2d(inner_nc * 3, outer_nc,
-                                        kernel_size=4, stride=2,
-                                        padding=1), use_spectral_norm)
-            down = [downrelu, downconv, downnorm]
-            # shift should be placed after uprelu
-            # NB: innerCos are placed before shift. So need to add the latent gredient to
-            # to former part.
-            up = [uprelu, innerCos, shift, upconv, upnorm]
-
-            model = down + [submodule] + up
-
-        self.model = nn.Sequential(*model)
-
-    def forward(self, x):
-        if self.outermost:  # if it is the outermost, directly pass the input in.
-            return self.model(x)
-        else:
-            x_latter = self.model(x)
-            _, _, h, w = x.size()
-            if h != x_latter.size(2) or w != x_latter.size(3):
-                x_latter = F.interpolate(x_latter, (h, w), mode='bilinear')
-            return torch.cat([x_latter, x], 1)  # cat in the C channel
-
 
 ################################### ***************************  #####################################
 ###################################      patch soft shift_net    #####################################
