c7b3ea36461f97ff1ba0bbbf8bf34f4edd77b8dd
Zhaoyi-Yan <yanzhaoyi@outlook.com>
2020-04-17 08:23:19 +0800

    add missing code

--- models/networks.py
+++ models/networks.py
@@ -93,8 +93,11 @@ def define_G(input_nc, output_nc, ngf, which_model_netG, opt, mask_global, norm=
     print('[CREATED] MODEL')
     if which_model_netG == 'unet_256':
         netG = UnetGenerator(input_nc, output_nc, 8, ngf, norm_layer=norm_layer, use_spectral_norm=use_spectral_norm)
-    if which_model_netG == 'easy_unet_256':
+    elif which_model_netG == 'easy_unet_256':
         netG = EasyUnetGenerator(input_nc, output_nc, ngf, norm_layer=norm_layer, use_spectral_norm=use_spectral_norm)
+    elif which_model_netG == 'face_unet_shift_triple':
+        netG = FaceUnetGenerator(input_nc, output_nc, innerCos_list, shift_list, mask_global, opt, \
+                                                         ngf, norm_layer=norm_layer, use_spectral_norm=use_spectral_norm)
     elif which_model_netG == 'unet_shift_triple':
         netG = UnetGeneratorShiftTriple(input_nc, output_nc, 8, opt, innerCos_list, shift_list, mask_global, \
                                                          ngf, norm_layer=norm_layer, use_spectral_norm=use_spectral_norm)
